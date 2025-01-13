22f09caff078b3b96a4ccd8e5815c21c7d72010b
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2025-01-13 21:29:44 +0100

    2. Creating and using environment variable

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
-let app=require('./app');  //import app object
-const port=3000;
+const dotenv=require('dotenv');  //npm install dotenv
+dotenv.config({path:'./config.env'}); // specify the path of config file.
+
+
+const app=require('./app');  //import app object
+const port=process.env.PORT_NUMBER || 3000;  // process.env contain all environment variable which is created inside config.env file.
 app.listen(port,()=>{
     console.log('listening on port no:',port)
 }); // its makes our application is ready to listen the incoming req. 
