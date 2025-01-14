205190a946bd268c058ddd9e7e5948a5a8cee92a
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2025-01-14 19:23:44 +0100

    4. created user schema and modelling for user registration

---
+++ server/models/users.js
@@ -0,0 +1,30 @@
+// this file contain:
+    // created user scheema, we created following schema, So mongoose will export this schema and tell that in DB create collection which is name as 'user' document
+    // and verify that document according to userSchema obj.
+
+const mongoose=require('mongoose');
+
+let userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    proflePic:{
+        type:String,
+        required:false
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model('users',userSchema); //create users document/collection in Db using userSchema verification.
