7136de9ceb625e7a87fe6df33a921d8e233c2b56
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2025-01-18 16:24:54 +0100

    13- Creating message schema and model which are used to sending send message api and create message collection in DB

---
+++ server/models/messages.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+
+const messageSchema=new mongoose.Schema({
+    // get the id of chat which member array contain sender and receiver , coming from chat collection
+    chatId:{
+        type:mongoose.Schema.Types.ObjectId,ref:'chat'
+    },
+    //who is sending message
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,ref:'users'
+    },
+    text:{
+        type:string,
+        required:true
+    },
+    read:{
+        type:Boolean
+    }
+})
+module.exports=mongoose.model('messages',messageSchema)
+// now this model is used for sending mesage API
