5111eea7064fe3155b6a55fc7c669311797c03d3
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2025-03-03 22:05:51 +0100

    2.signup:create user object in signup page and console it when we click on submit button.(useState) also create seperate tailwind css file.

--- client/src/signup/index.js
+++ client/src/signup/index.js
@@ -1,43 +1,66 @@
-
+import { useState } from "react";
+import './signUp.css';
 
 function Signup() {
+    const [user, setUser]=useState({
+        // this schema exactly same in backend user schema.
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:''
+    });
+
+    function onFormSubmit(event){
+        event.preventDefault();
+        console.log(user);
+    }
     return (
-        <div className=" relative h-screen background-image bg-cover ">
-            <div className="absolute inset-0 bg-red-500 bg-opacity-50">
-                <div className="flex  justify-center items-center min-h-screen w-[90%]">
-                    <div className="bg-white h-auto w-[90%] max-w-[400px] rounded-md p-4 shadow-lg">
-                        <div className="font-extrabold text-center text-2xl m-2 p-1">
+        <div className="container background-image ">
+            <div className="container-back-color">
+                <div className="card-layout">
+                    <div className="card">
+                        <div className="card-label">
                             Create Account
                         </div>
+                        <form  className="form" onSubmit={onFormSubmit}>
                         {/* name input */}
-                        <div className="flex md:flex-row flex-col gap-1 justify-center mb-1 m-0">
-
-                            <input className="bg-slate-200 border-none outline-none h-10 w-full px-4  text-sm m-0 rounded-sm"
-                                type="text" alt="firstname" placeholder="Enter Firstname.."></input>
-                            <input className="bg-slate-200 border-none outline-none h-10 w-full px-4 text-sm m-0 rounded-sm"
-                                type="text" alt="lastname" placeholder="Enter Lastname.."></input>
+                        <div className="column">
+                            
+                            <input
+                                type="text" alt="firstname" placeholder="Enter Firstname.."
+                                value={user.firstName}
+                                //all value from user and get the value of firstname
+                                onChange={(e)=>setUser({...user,firstName:e.target.value})}></input>
+                            <input
+                                type="text" alt="lastname" placeholder="Enter Lastname.."
+                                value={user.lastName}
+                                onChange={(e)=>setUser({...user,lastName:e.target.value})}></input>
 
 
                         </div>
 
                         {/* email and password */}
-                        <div className="flex flex-col space-y-1 md:space-y-1 mb-2">
-                            <input className="bg-slate-200 border-none outline-none h-10 w-full px-4  text-sm rounded-sm"
-                                type="email" alt="Email" placeholder="Enter Email.."></input>
-                            <input className="bg-slate-200 border-none outline-none h-10 w-full px-4  text-sm rounded-sm"
-                                type="password" alt="Password" placeholder="Enter Password.."></input>
+                        <div className="email-column">
+                            <input
+                                type="email" alt="Email" placeholder="Enter Email.."
+                                value={user.email}
+                                onChange={(e)=>setUser({...user,email:e.target.value})}></input>
+                            <input
+                                type="password" alt="Password" placeholder="Enter Password.."
+                                value={user.password}
+                                onChange={(e)=>setUser({...user,password:e.target.value})}></input>
                         </div>
 
                         {/* signup button */}
                         <div className="mb-2">
-                            <button className="w-full bg-red-500 text-white py-1 rounded-md font-semibold hover:bg-blue-500 focus:outline-none 
-                                                focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 duration-300"
-                                    type="button">Sign Up</button>
+                            <button>Sign Up</button>
                         </div>
-                        <div className="mb-2 flex justify-center  gap-2">
-                            <p className="text-gray-600">Already have an account?</p>
-                            <p className="text-blue-800 underline cursor-pointer">Login Here</p>
+                        </form>
+                        <div className="card-terms">
+                            <p>Already have an account?</p>
+                            <p className="p-login">Login Here</p>
                         </div>
+                        
                     </div>
                 </div>
             </div>
