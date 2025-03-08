c24246eade20cf9ae4828c40b5c383b5411aea6b
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2025-03-08 14:42:10 +0100

    8.Fetched Logged User Details: 1.write asnc function to write api call to get the details of curretly logged in user. 2.this is protected api so we need to send authentication token while sending req. tha we are already doing in axiosInstance in apicalls/index.js file. 3.thencall this methos from protected route comp and get the user object. and at the first time app is render then call useEffect hook and check if token is available then get the user object othereise return login page. and in return() use ?.user bcoz at first time we get null user .

--- client/src/components/protectedRoute.js
+++ client/src/components/protectedRoute.js
@@ -1,17 +1,35 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { getLoggedInUsers } from "../apicalls/users";
 
 function ProtectedRoute({children}){
+    let[user,setUsers]=useState(null);
     const navigate=useNavigate();
+
+    const getLoggedUsers=async()=>{
+        let response=null;
+        try{
+            response=await getLoggedInUsers();
+            if(response.success){
+                setUsers(response.data);
+            }else{
+                window.location.href='/login';
+            }
+        }catch(error){
+            navigate('/login');
+        }
+    }
     useEffect(()=>{
         if(localStorage.getItem('token')){
             //get the current user details
+            getLoggedUsers();
         }else{
             navigate('/login')
         }
-    })
+    },[])
     return(
         <div>
+            <p>Name:{user?.firstName+' '+user?.lastName}</p>
             {children}
         </div>
     )
