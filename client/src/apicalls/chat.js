9f43a25ac1efb60f378f51e07640b442258cf3fa
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2025-03-24 18:55:03 +0100

    15Starting a new chat 1. create chat with post req. 2.send members array which contain search _user_id and current loggedin user_id as a payload. 3. currentuser_id present ij store so we get from it. and search user id will get when we search user and display firstname and lastname.

--- client/src/apicalls/chat.js
+++ client/src/apicalls/chat.js
@@ -8,3 +8,12 @@ export const getAllChats=async()=>{
         return error;
     }
 }
+//create chat
+export const createNewChat=async(members)=>{
+    try{
+        const response=await axiosInstance.post('/api/chat/create_chat_between_members',{members})
+        return response.data;
+    }catch(error){
+        return error;
+    }
+}
--- client/src/home/components/sidebar/UserList/userlist.js
+++ client/src/home/components/sidebar/UserList/userlist.js
@@ -1,13 +1,41 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import './userlist.css';
+import toast from "react-hot-toast";
+import { hideLoader, showLoader } from "../../../../redux/loaderSlice";
+import { createNewChat } from "../../../../apicalls/chat";
+import { setAllChats } from "../../../../redux/userSlice";
 
 function UserList({ searchKey }) {
-    const { allUsers, allChats } = useSelector(state => state.userReducer);
+    //create alise for user as a currentUser
+    const { allUsers, allChats, user:currentUser } = useSelector(state => state.userReducer);
     // console.log(allUsers)
+    const dispatch=useDispatch();
     function formatName(user) {
         let firstName = user?.firstName.charAt(0).toUpperCase() + user?.firstName.slice(1).toLowerCase();
         let lastName = user?.lastName.charAt(0).toUpperCase() + user?.lastName.slice(1).toLowerCase();
         return firstName + ' ' + lastName;
+    }
+
+    //call to create new chat api function when we click on start chat button
+    const startNewChat= async (searchUserId)=>{
+        let response=null;
+        try{
+            dispatch(showLoader());
+            //pass the members array which contain current user id and search use id.
+            response=await createNewChat([currentUser._id,searchUserId]);
+            dispatch(hideLoader());
+            if(response.success){
+                toast.success(response.message);
+                //create newChat variable, and update the store with allchat slice which contain all members array who is doing chat
+                const newChat=response.data;
+                const updateWithNewChat=[...allChats,newChat] //this contain all members 
+                //update in the store, now allChats contain all the members who are doing chats.
+                dispatch(setAllChats(updateWithNewChat));
+            }
+        }catch(error){
+            toast.error(response.message);
+            dispatch(hideLoader())
+        }
     }
     return (
         allUsers.filter(user =>
@@ -39,7 +67,9 @@ function UserList({ searchKey }) {
                     </div>
                     {!allChats.find(chat =>  chat.members.includes(user._id) ) &&
                         <div className="user-start-chat">
-                            <button className="user-start-chart-btn">Start chat</button>
+                            <button className="user-start-chart-btn"
+                            // pass search user as a param, allUser contain logic of searchuser
+                            onClick={()=>startNewChat(user._id)}>Start chat</button> 
                         </div>
                     }
                 </div>
