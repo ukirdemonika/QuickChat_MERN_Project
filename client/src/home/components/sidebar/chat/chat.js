4f0ff2103fb9f6c06a6b2b66b218b81a06f34dde
monikau-vgs <36798308+ukirdemonika@users.noreply.github.com>
2025-04-08 23:12:48 +0200

    18. get all messages from api- fetaching all messages

--- client/src/home/components/sidebar/chat/chat.js
+++ client/src/home/components/sidebar/chat/chat.js
@@ -1,55 +1,77 @@
-import react from 'react';
+import react, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './chat.css';
-import {createNewMessage} from "../../../../apicalls/messages";
-import { showLoader,hideLoader } from "../../../../redux/loaderSlice";
+import { createNewMessage, getAllMessages } from "../../../../apicalls/messages";
+import { showLoader, hideLoader } from "../../../../redux/loaderSlice";
 import toast from 'react-hot-toast';
-function ChatArea(){
-    const{selectedChat,user:currentUser}=useSelector(state=>state.userReducer);
+function ChatArea() {
+    const { selectedChat, user: currentUser } = useSelector(state => state.userReducer);
     //chat is selected chat, and user is current user.
     //selectedUserChat is the chat which is selected by the user, and it is the member of the selected chat.
-    const selectedUserChat=selectedChat.members.find(m=>m._id !== currentUser._id);//get chat object of selected user. 
-    const [message,setMessage]=react.useState('');
-    const dispatch=useDispatch()
+    const selectedUserChat = selectedChat.members.find(m => m._id !== currentUser._id);//get chat object of selected user. 
+    const [message, setMessage] = react.useState('');
+    const [allMessages, setAllMessages] = react.useState([]);
+    const dispatch = useDispatch()
 
-    async function createNewMessageFromApi(){
-        let response=null;
-        try{
-            
-            let newMessage={
-                chatId:selectedChat._id,
-                sender:currentUser._id,
-                text:message
+    async function sendMessageFromApi() {
+        let response = null;
+        try {
+
+            let newMessage = {
+                chatId: selectedChat._id,
+                sender: currentUser._id,
+                text: message
             }
             dispatch(showLoader());
-            response=await createNewMessage(newMessage);
+            response = await createNewMessage(newMessage);
             dispatch(hideLoader());
-            if(response.success){
+            if (response.success) {
                 setMessage('');
-        }
-    }catch(error){
+            }
+        } catch (error) {
             dispatch(hideLoader());
             toast.error(response.message);
         }
     }
-    return(
+
+    async function getAllMessagesFromDB() {
+        let response = null;
+        try {
+            dispatch(showLoader());
+            response = await getAllMessages(selectedChat._id);
+            dispatch(hideLoader());
+            if (response.success) {
+                setAllMessages(response.data);
+            }
+        } catch (error) {
+            dispatch(hideLoader());
+            toast.error(response.message);
+        }
+    }
+    //get all messages from db when selected chat is changed & initially when page load.
+    //selected chat is the chat which is selected by the user.
+    useEffect(() => {
+        getAllMessagesFromDB();
+    }, [selectedChat]); //when selected chat is changed, then get all messages from db.
+
+    return (
         <>
             {selectedChat && <div className='chat-area'>
                 <div className='chat-header'>
-                {selectedUserChat.firstName} {selectedUserChat.lastName}
+                    {selectedUserChat.firstName} {selectedUserChat.lastName}
                 </div>
                 <div className='chat-area'>
-                chat text
+                    chat text
                 </div>
                 <div className='send-message-div'>
                     <input type='text' className='send-message-input' placeholder='Type a message...'
-                    value={message}
-                    onChange={(e)=>{setMessage(e.target.value)}} />    
-                    <button className="fa fa-paper-plane send-message-btn" 
-                            aria-hidden="true" onClick={createNewMessageFromApi}></button>
+                        value={message}
+                        onChange={(e) => { setMessage(e.target.value) }} />
+                    <button className="fa fa-paper-plane send-message-btn"
+                        aria-hidden="true" onClick={sendMessageFromApi}></button>
 
                 </div>
-                </div>
+            </div>
             }
         </>
     )
